8a8cd6801a69b76f09013bb2a3cfa6e34e2df96f
optics915 <49119501+optics915@users.noreply.github.com>
2020-07-16 15:27:40 +0800

    first commit

---
+++ src/main/java/com/lrm/BlogApplication.java
@@ -0,0 +1,53 @@
+package com.lrm;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+//import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+	}
+
+
+//	HTTPS协议
+//拦截所有请求
+	@Bean
+	public EmbeddedServletContainerFactory servletContainer() {
+		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint constraint = new SecurityConstraint();
+				constraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				constraint.addCollection(collection);
+				context.addConstraint(constraint);
+			}
+		};
+		tomcat.addAdditionalTomcatConnectors(httpConnector());
+		return tomcat;
+	}
+
+	@Bean
+	public Connector httpConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setScheme("http");
+		//Connector监听的http的端口号
+		connector.setPort(80);
+		connector.setSecure(false);
+		//监听到http的端口号后转向到的https的端口号
+		connector.setRedirectPort(443);
+		return connector;
+	}
+}
